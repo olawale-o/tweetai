dd843a17c3da00b1850cbb1f30636dc32c756f67
olawale-o <omoogunolawale@gmail.com>
2024-08-22 01:41:28 +0100

    Setup server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const handleError = require("../common/error-handler");
 
 const corsOption = {
   origin: "http://localhost:3000",
@@ -15,8 +14,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors(corsOption));
 
-app.use(async (err, _req, res) => {
-  await handleError(err, res);
-});
+app.use("/api/v1/bot", require("./bot"));
+
+// app.use(async (err, _req, res) => {
+//   await handleError(err, res);
+// });
 
 module.exports = app;
