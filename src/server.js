046659fbd5214f3594521a49dd98a28662122ec9
olawale-o <omoogunolawale@gmail.com>
2024-08-24 00:55:31 +0100

    Restrict requests to 5 requests per minutes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 
 const agenda = require("./jobs");
 
@@ -9,32 +10,17 @@ const corsOption = {
   credential: true,
 };
 
-// const mongoConnectionString = "mongodb://127.0.0.1/agenda";
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  limit: 5,
+});
 
-// const agenda = new Agenda({
-//   db: {
-//     address: mongoConnectionString,
-//     options: {
-//       useUnifiedTopology: true,
-//     },
-//   },
-//   processEvery: "1 minute",
-//   maxConcurrency: 20,
-// });
+// (async function () {
+//   await agenda.start();
 
-// agenda.define("create_bots_every_1_mins", createUsers);
-
-// agenda.define("get_hourly_created_users", async () => {
-//   const totalBots = await db.select({ value: count(bots.id) }).from(bots);
-//   console.log(`current total bots ${totalBots}`);
-// });
-
-(async function () {
-  await agenda.start();
-
-  await agenda.every("1.2 minutes", "create-bots");
-  await agenda.every("60 minutes", "hourly-bots");
-})();
+//   await agenda.every("1.2 minutes", "create-bots");
+//   await agenda.every("60 minutes", "hourly-bots");
+// })();
 
 const app = express();
 
@@ -43,6 +29,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors(corsOption));
 
+app.use(limiter);
 app.use("/api/v1/bot", require("./bot"));
 
 // app.use(async (err, _req, res) => {
