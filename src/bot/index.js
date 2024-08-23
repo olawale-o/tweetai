749b2af22abc302c63369dc5194fb38d4900c17c
olawale-o <omoogunolawale@gmail.com>
2024-08-23 18:19:21 +0100

    Create bot route

--- src/bot/index.js
+++ src/bot/index.js
@@ -1,16 +1,10 @@
+const handler = require("./handler");
+
 const router = require("express").Router();
 
-router.get("/", async (req, res, next) => {
-  console.log("get all bots");
-  res.sendStatus(200);
-});
-router.get("/:id/posts", async (req, res, next) => {
-  console.log("get a bot's posts");
-  res.sendStatus(200);
-});
-router.get("/:id/posts/:id/comments", async (req, res, next) => {
-  console.log("get a bot's post comments");
-  res.sendStatus(200);
-});
+router.get("/", handler.index);
+
+router.get("/:botId/posts", handler.getPosts);
+router.get("/:postId/comments", handler.getComments);
 
 module.exports = router;
