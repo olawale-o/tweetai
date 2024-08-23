60258711f7bd0acb04e91fbba60cf024e01f0cbd
olawale-o <omoogunolawale@gmail.com>
2024-08-23 18:19:04 +0100

    Create jobs

---
+++ src/jobs/index.js
@@ -0,0 +1,26 @@
+const Agenda = require("agenda");
+const { allDefinitions } = require("./definitions");
+
+const mongoConnectionString = "mongodb://127.0.0.1/agenda";
+
+const agenda = new Agenda({
+  db: {
+    address: mongoConnectionString,
+    options: { useUnifiedTopology: true },
+  },
+  processEvery: "1 minute",
+  maxConcurrency: 20,
+});
+
+// listen for the ready or error event.
+agenda
+  .on("ready", () => console.log("Agenda started!"))
+  .on("error", () => console.log("Agenda connection error!"));
+
+// define all agenda jobs
+allDefinitions(agenda);
+
+// logs all registered jobs
+console.log({ jobs: agenda._definitions });
+
+module.exports = agenda;
