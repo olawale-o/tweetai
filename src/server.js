0ca858c33dca52a1b3410893c25d9a64e7719ca1
olawale-o <omoogunolawale@gmail.com>
2024-08-23 18:09:26 +0100

    Setup app server

--- src/server.js
+++ src/server.js
@@ -2,10 +2,39 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+const agenda = require("./jobs");
+
 const corsOption = {
   origin: "http://localhost:3000",
   credential: true,
 };
+
+// const mongoConnectionString = "mongodb://127.0.0.1/agenda";
+
+// const agenda = new Agenda({
+//   db: {
+//     address: mongoConnectionString,
+//     options: {
+//       useUnifiedTopology: true,
+//     },
+//   },
+//   processEvery: "1 minute",
+//   maxConcurrency: 20,
+// });
+
+// agenda.define("create_bots_every_1_mins", createUsers);
+
+// agenda.define("get_hourly_created_users", async () => {
+//   const totalBots = await db.select({ value: count(bots.id) }).from(bots);
+//   console.log(`current total bots ${totalBots}`);
+// });
+
+(async function () {
+  await agenda.start();
+
+  await agenda.every("1.2 minutes", "create-bots");
+  await agenda.every("60 minutes", "hourly-bots");
+})();
 
 const app = express();
 
