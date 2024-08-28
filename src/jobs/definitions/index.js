3d530f0d52648df38fcad7fa1bec7fff1090c7e1
olawale-o <omoogunolawale@gmail.com>
2024-08-28 10:26:45 +0100

    Add more definitions

--- src/jobs/definitions/index.js
+++ src/jobs/definitions/index.js
@@ -1,5 +1,8 @@
-const { botDefinitions } = require("./bot");
+const { botDefinitions, sendTotalBots } = require("./bot");
 
-const allDefinitions = (agenda) => botDefinitions(agenda);
+const definitions = [botDefinitions, sendTotalBots];
+
+const allDefinitions = (agenda) =>
+  definitions.forEach((definition) => definition(agenda));
 
 module.exports = { allDefinitions };
