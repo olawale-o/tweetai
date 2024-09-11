ef6c6fcbe8222038dc8662966a3de837d0ea0b52
olawale-o <omoogunolawale@gmail.com>
2024-09-11 14:35:42 +0100

    Uninstall nodemon

--- src/service/backup.js
+++ src/service/backup.js
@@ -1,13 +1,24 @@
 const Rsync = require("rsync");
 
+const syncProgram = process.platform === "win32" ? "robocopy" : "rsync";
+
 const rsync = new Rsync()
+  .executable(syncProgram)
   .flags("a")
   .source("source/")
   .destination("destination/");
 
 const backupService = () => {
   rsync.execute((error, code, cmd) => {
-    console.log("backup completed with status code: " + code);
+    let result = null;
+
+    if (error) {
+      result = `Code ${code} ${error?.message}`;
+    } else {
+      result = "Backup complete";
+    }
+    const currentDate = new Date().toISOString();
+    process.stdout.write(`${currentDate}: ${result}\n`);
   });
 };
 module.exports = backupService;
